7fdd184dcc358c18b29599b036dccd51608a795c
Kyle Baker <kydoggcodes@gmail.com>
2025-11-01 17:14:20 -0400

    feat: implement home page with movie carousels
    Display upcoming, top-rated, and popular movies in carousels.
    Fetches data from TMDB API using server-side rendering.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-	return <main className=""></main>;
+import MoviesCarousel from "@/components/MoviesCarousel";
+import {
+	getPopularMovies,
+	getTopRatedMovies,
+	getUpComingMovies,
+} from "@/lib/getMovies";
+
+export default async function Home() {
+	const upcomingMovies = await getUpComingMovies();
+	const topRatedMovies = await getTopRatedMovies();
+	const popularMovies = await getPopularMovies();
+	return (
+		<main className="">
+			{/* TODO: Carousel banner */}
+			<div className="flex flex-column space-y-2 xl:-mt-48">
+				<MoviesCarousel movies={upcomingMovies} title="Upcoming..." />
+				<MoviesCarousel movies={topRatedMovies} title="Top Rated..." />
+				<MoviesCarousel movies={popularMovies} title="Popular..." />
+			</div>
+		</main>
+	);
 }
