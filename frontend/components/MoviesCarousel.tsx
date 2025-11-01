05d4891a76e480e5109e4ed9def5a9d9c2d9ad3f
Kyle Baker <kydoggcodes@gmail.com>
2025-11-01 17:14:08 -0400

    feat: add movie components and TMDB utilities
    - Add getImagePath helper for TMDB image URLs
    - Add getMovies with upcoming/top-rated/popular fetchers
    - Add MovieCard component with hover effects
    - Add MoviesCarousel component for horizontal scrolling
    - Format GenreDropdown console.error for readability

---
+++ frontend/components/MoviesCarousel.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Movie } from "@/typings";
+import MovieCard from "./MovieCard";
+import { cn } from "@/lib/utils";
+
+type Props = {
+	title?: string;
+	movies: Movie[];
+	isVertical?: boolean;
+};
+
+function MoviesCarousel({ title, movies, isVertical }: Props) {
+	return (
+		<div className="z-50">
+			<h2>{title}</h2>
+
+			<div
+				className={cn(
+					"flex space-x-4 overflow-scroll px-5 py-5 scrollbar-hide"
+				)}
+			>
+				{movies?.map((movie) => (
+					<MovieCard key={movie.id} movie={movie} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default MoviesCarousel;
