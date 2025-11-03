2df6b9882c33d6f273b539077ee04f138c5ec0c1
Kyle Baker <kydoggcodes@gmail.com>
2025-11-03 12:19:49 -0500

    fix: add fallback to MovieCard image src to prevent undefined error

--- frontend/components/MovieCard.tsx
+++ frontend/components/MovieCard.tsx
@@ -10,7 +10,7 @@ function MovieCard({ movie }: { movie: Movie }) {
 
 			<Image
 				className="w-fit lg:min-w-[400px] h-56 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm"
-				src={getImagePath(movie.backdrop_path || movie.poster_path)}
+				src={getImagePath(movie.backdrop_path || movie.poster_path || "")}
 				alt={movie.title}
 				width={1920}
 				height={1080}
